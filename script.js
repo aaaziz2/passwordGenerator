813ddcfdb6f8ceae540fea6780cacf3dbc143973
Aumio Aziz <aumio.aziz@gmail.com>
2022-06-26 17:39:40 -0500

    Added password requirement checks Added reset of variables at the beginning of the writePassword function so it can be re run

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ var charLowSet = "abcdefghijklmnopqrstuvxyz"
 var charUppSet = charLowSet.toUpperCase()
 var charNumSet = "0123456789"
 var charSpeSet = "!@#$%^&*()\"\'+,-./:;<>=?[]\\_`{}|~"
+var requirement = [0,0,0,0]
 
 // Write password to the #password input
 function writePassword() {
-
+  //reset the variables on re-run
+  requirement = [0,0,0,0]
+  charLow = false;
+  charUpp = false;
+  charNum = false;
+  charSpe = false;
   // // reset when password button is clicked again adding if time allows
   // if(document.querySelector("#password").value != "Your Secure Password"){
   //   document.
@@ -51,7 +57,17 @@ function writePassword() {
       alert("Invalid Length! Please try again")
       return
     }
-    var password = generatePassword();
+    
+    var password = generatePassword()
+
+    console.log(requirement.includes(1))
+
+    console.log(requirement)
+    // run until requirements are met
+    while(requirement.includes(1)){
+      password = generatePassword()
+    }
+
     var passwordText = document.querySelector("#password");
   }
   // if cancel is selected the password is not generated
@@ -65,41 +81,56 @@ function generatePassword(){
   // default values of password and set of allowed characters
   var password = ""
   var passSet = ""
+  requirement = [0,0,0,0]
   // depending on what the user entered add to the total set of allowed characters
   if(charLow){
-    passSet = passSet.concat(charLowSet);
-    // console.log(passSet);
+    passSet = passSet.concat(charLowSet)
+    requirement[0]++
   }
   if(charUpp){
-    passSet = passSet.concat(charUppSet);
-    // console.log(passSet);
+    passSet = passSet.concat(charUppSet)
+    requirement[1]++
   }
   if(charNum){
     passSet = passSet.concat(charNumSet)
-    // console.log(passSet)
+    requirement[2]++
   }
   if(charSpe){
     passSet = passSet.concat(charSpeSet)
-    // console.log(passSet)
+    requirement[3]++
   }
-  // making sure that the password is correct length per user request for debugging
-  // console.log(passLen)
 
   // Adding random characters from the total allowed characters
   for(i=0;i<passLen;i++){
     // generate random number between 0 and the full length of the password character set
     var randomChar = Math.floor(Math.random() * passSet.length)
     
-    // console.log(randomChar)
-    // console.log(passSet.charAt(randomChar))
+    var currentChar = passSet.charAt(randomChar)
+   
+    // check if 
+    if(requirement[0] == 1){
+      if(charLowSet.includes(currentChar)){
+        requirement[0]++
+      }
+    }
+    if(requirement[1] == 1){
+      if(charUppSet.includes(currentChar)){
+        requirement[1]++
+      }
+    }
+    if(requirement[2] == 1){
+      if(charNumSet.includes(currentChar)){
+        requirement[2]++
+      }
+    }
+    if(requirement[3] == 1){
+      if(charSpeSet.includes(currentChar)){
+        requirement[3]++
+      }
+    }
     
     // add the random char to the password
-    password += passSet.charAt(randomChar)
-  }
-
-  // Check to make sure all characters are included !!needs work
-  for(j=0; j<password.length; j++){
-
+    password += currentChar
   }
 
   return password
